70f3c6126277a89bd1f8da6cd26505f2e148887a
Christo150298 <christopherjavier1998@gmail.com>
2025-04-20 19:37:22 +0200

    Practica 1 Modulo 3

---
+++ M3PO1_Christopher_Alvarez/script.js
@@ -0,0 +1,12 @@
+// Datos fijos
+let ladoCuadrado = 5; // Lado del cuadrado
+let baseTriangulo = 8; // Base del triángulo
+let alturaTriangulo = 4; // Altura del triángulo
+
+// Cálculos
+let areaCuadrado = ladoCuadrado * ladoCuadrado;
+let areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+// Resultados por consola
+console.log(`La superficie del cuadrado es: ${areaCuadrado}`);
+console.log(`La superficie del triángulo es: ${areaTriangulo}`);
